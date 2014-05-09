b2385e3e2bccaed8875f439d7be5b80d90ebf6ba
Peiyi Chen <pchen@nb-pchen.(none)>
2014-05-09 15:02:20 -0400

    first commit

---
+++ plot_demo/src/demo.cpp
@@ -0,0 +1,262 @@
+#include "ros/ros.h"
+#include "nav_msgs/Path.h"
+#include "ecl/geometry.hpp"
+#include "geometry_msgs/PoseStamped.h"
+#include "geometry_msgs/Pose.h"
+#include "plot_tool/PlotPath.h"
+#include "plot_tool/PlotPose.h"
+
+ros::ServiceClient path_srv_h;
+ros::ServiceClient pose_srv_h;
+
+/**
+ * Construct_Path_Msg function
+ * Used to populate a nav_msgs::Path given a list of x and y coordinates
+ * @param x double* pointer to array containing x coordinates
+ * @param y double* pointer to array containing y coordinates
+ * @return msg the constructed nav_msgs::Path message
+ */
+nav_msgs::Path Construct_Path_Msg(double* x, double *y, int length)
+{
+	nav_msgs::Path msg;
+	std::vector<geometry_msgs::PoseStamped> poses(length);
+	for (int i = 0; i < length; i++)
+	{
+		poses.at(i).pose.position.x = x[i];
+		poses.at(i).pose.position.y = y[i];
+	}
+	msg.poses = poses;
+	return msg;
+}
+
+/**
+ * run_demo function
+ * This function plots what you see in the demo
+ * This is an ugly hard coded mess because its only purpose is to demo plot_tool
+ * It is slapped together from code used to test viability of ECL's spline interpolator
+ */
+void run_demo()
+{
+	plot_tool::PlotPath path_srv;
+
+	//clear graph
+	path_srv.request.append=false;
+	for (int i = 0; i < 7; i++)
+	{
+		path_srv.request.series=i;
+		path_srv_h.call(path_srv);
+	}
+
+	ros::Duration(2).sleep();
+
+	//draw walls using nav_msg::Path
+	double wall_x[] = {0, 0, 4, 4, 10, 10, 6, 6, 0};
+	double wall_y[] = {0, 2, 2, 10, 10, 8, 8, 0, 0};
+	nav_msgs::Path wall_msg = Construct_Path_Msg(wall_x, wall_y, sizeof(wall_x)/sizeof(double));
+	//draw walls
+	path_srv.request.msg = wall_msg;
+	path_srv.request.series=6;
+	path_srv.request.append=false;
+	path_srv.request.symbol='-';
+	path_srv.request.symbol_size=10;
+	path_srv_h.call(path_srv);
+
+	ros::Duration(2).sleep();
+
+	//draw initial wps using nav_msg::Path
+	int wp_total = 4;
+	double * wp_x = (double*)malloc(sizeof(double)*wp_total);
+	double * wp_y = (double*)malloc(sizeof(double)*wp_total);
+	wp_x[0] = 1; wp_x[1] = 5; wp_x[2] = 5; wp_x[3] = 9;
+	wp_y[0] = 1; wp_y[1] = 1; wp_y[2] = 9; wp_y[3] = 9;
+	nav_msgs::Path wp_msg = Construct_Path_Msg(wp_x, wp_y, wp_total);
+
+	//draw wps as points
+	path_srv.request.msg = wp_msg;
+	path_srv.request.series=0;
+	path_srv.request.append=false;
+	path_srv.request.symbol='o';
+	path_srv.request.symbol_size=10;
+	path_srv_h.call(path_srv);
+
+	ros::Duration(2).sleep();
+
+	//connect wp as a line
+	path_srv.request.symbol='-';
+	path_srv.request.append=true;
+	path_srv_h.call(path_srv);
+
+	ros::Duration(2).sleep();
+
+	//spline fitting
+	bool safe = false;
+	double * x_ptr;
+	double * y_ptr;
+	x_ptr = wp_x;
+	y_ptr = wp_y;
+	int spline_series = 2;
+	nav_msgs::Path spline_msg;
+	ecl::CubicSpline spline_x;
+	ecl::CubicSpline spline_y;
+	//iterate until we get a collision free spline
+	while (!safe)
+	{
+		safe = true;
+
+		//find a spline to fit the linear path
+		ecl::Array<double> t_set(wp_total);
+		ecl::Array<double> x_set(wp_total);
+		ecl::Array<double> y_set(wp_total);
+		for (int i = 0; i < wp_total; i++)
+		{
+			t_set[i] = (double)i;
+			x_set[i] = x_ptr[i];
+			y_set[i] = y_ptr[i];
+		}
+
+		//draw points used to construct spline
+		nav_msgs::Path spline_pts_msg = Construct_Path_Msg(x_ptr, y_ptr, wp_total);
+		path_srv.request.msg = spline_pts_msg;
+		path_srv.request.series=(++spline_series%5)+1;
+		path_srv.request.append=false;			
+		path_srv.request.symbol='d';
+		path_srv.request.symbol_size=10;
+		path_srv_h.call(path_srv);
+
+		ros::Duration(1).sleep();
+
+		//spline fit with ECL geometry
+		spline_x = ecl::CubicSpline::Natural(t_set,x_set);
+		spline_y = ecl::CubicSpline::Natural(t_set,y_set);
+		int spline_pts = (wp_total-1)*100+1;
+		double sx[spline_pts];
+		double sy[spline_pts];
+		for (int i = 0; i < spline_pts; i++)
+		{
+			sx[i] = spline_x(i/100.0);
+			sy[i] = spline_y(i/100.0);
+
+			//crappy implicit collision check, this should be replaced with something better
+			if (sx[i] <=4.1 && sy[i] >= 1.9)
+			{
+				safe = false;
+			}
+			else if (sx[i] > 5.9 && sy[i] <= 8.1)
+			{
+				safe = false;
+			}
+			else if (sx[i] <= 0.1 || sx[i] >= 9.9)
+			{
+				safe = false;
+			}
+			else if (sy[i] <= 0.1 || sy[i] >= 9.9)
+			{
+				safe = false;
+			}
+		}
+		spline_msg = Construct_Path_Msg(sx, sy, sizeof(sx)/sizeof(double));
+	
+		//draw spline
+		path_srv.request.msg = spline_msg;
+		path_srv.request.series=(spline_series%5)+1;
+		path_srv.request.append=true;			
+		path_srv.request.symbol='-';
+		path_srv.request.symbol_size=10;
+		path_srv_h.call(path_srv);
+
+		if (!safe)
+		{
+			//add more points to force spline closer to path
+			wp_total = wp_total*2-1;
+			double * new_wpx = (double*)malloc(sizeof(double)*wp_total);
+			double * new_wpy = (double*)malloc(sizeof(double)*wp_total);
+			for (int i = 0; i < wp_total; i++)
+			{
+				if (i%2 == 0)
+				{
+					new_wpx[i] = x_ptr[i/2];
+					new_wpy[i] = y_ptr[i/2];
+				}	
+				else
+				{
+					new_wpx[i] = (x_ptr[(int)i/2]+x_ptr[(int)(i/2)+1])/2.0;
+					new_wpy[i] = (y_ptr[(int)i/2]+y_ptr[(int)(i/2)+1])/2.0;
+				}
+			}
+
+			free(x_ptr);
+			free(y_ptr);
+			x_ptr = new_wpx;
+			y_ptr = new_wpy;
+		}
+
+		ros::Duration(2).sleep();
+	}
+
+	//clear graph except the spline
+	nav_msgs::Path empty_path_msg;
+	path_srv.request.append=false;
+	for (int i = 0; i < 6; i++)
+	{
+		if (i!=(spline_series%5)+1)
+		{
+			path_srv.request.msg=empty_path_msg;
+		}
+		else
+		{
+			path_srv.request.msg=spline_msg;
+		}
+		path_srv.request.series=i;
+		path_srv_h.call(path_srv);
+	}
+
+	ros::Duration(2).sleep();
+
+	//draw the vehicle traversing the path using pose messages
+	geometry_msgs::Pose vehicle_msg;
+
+	plot_tool::PlotPose pose_srv;
+	pose_srv.request.msg = vehicle_msg;
+	pose_srv.request.series=1;
+	pose_srv.request.append=false;			
+	pose_srv.request.symbol='s';
+	pose_srv.request.symbol_size=50;
+
+	int t_max = wp_total-1;
+	int steps = t_max*20+1;
+	
+	//interpolate existing segments to add more control points for a tighter spline
+	for (int i = 0; i < steps; i++)
+	{
+		pose_srv.request.msg.position.x = spline_x(i/20.0);
+		pose_srv.request.msg.position.y = spline_y(i/20.0);
+
+		pose_srv_h.call(pose_srv);
+
+		ros::Duration(0.05).sleep();
+	}
+
+	ros::Duration(2).sleep();
+}
+
+/**
+ * Main Function 
+ * Used to bring up the ROS node and subscribe to plotting services.
+ */
+int main(int argc, char **argv)
+{
+	ros::init(argc, argv, "cubic_spline_interpolation");
+  	ros::NodeHandle n;
+
+	//subscribe to the plotting services
+  	path_srv_h = n.serviceClient<plot_tool::PlotPath>("/plot_tool/draw_path");
+	pose_srv_h = n.serviceClient<plot_tool::PlotPose>("/plot_tool/draw_pose");
+
+	while (!ros::isShuttingDown())
+	{
+		run_demo();
+		ros::spinOnce();
+	}
+
+  	return 0;
+}
---
+++ plot_tool/scripts/plot_path.py
@@ -0,0 +1,198 @@
+#!/usr/bin/env python
+
+##	@package plot_tool
+#	This tool is offers a ROS service that draws 2D parametric plots
+#	The tool currently supports geometry_msgs::Pose and nav_msgs::Path
+#	The plotting is done using pyqtgraph v0.9.8
+
+import rospy
+import pyqtgraph as pg
+from pyqtgraph.Qt import QtGui, QtCore
+from nav_msgs.msg import Path 
+from geometry_msgs.msg import Pose
+from plot_tool.srv import *
+
+#	This is a global graph object, we need it global so your service handlers can access the graph
+graph_obj = None
+
+##	Class CustomGraphicsWindow
+#	We use this class to override the closeEvent function of the base pg.GraphicsWindow
+#	This is used to reopen the window, and redraw all the content, whenever the graph window is closed.
+class CustomGraphicsWindow(pg.GraphicsWindow):
+	##	Function closeEvent
+	#	This overrides the closeEvent function in the base class. This function is invoked automatically by QTGui when the window closes.
+	#	@param ev This is the event object (i.e. window close). 
+	def closeEvent(self,ev):
+		# recreate the graph window
+		graph_obj.win = CustomGraphicsWindow()
+		graph_obj.win.setWindowTitle('Plot Tool')
+		graph_obj.graph = graph_obj.win.addPlot()
+		graph_obj.graph.showGrid(x=True, y=True)
+		# iterate through the current plots, and readd them to the graph GUI
+		for s in graph_obj.plot_tracker:
+			for p in s:
+				graph_obj.graph.addItem(p)
+
+##	Class Graph_Drawer 
+#	This class manages all the pyqtgraph plotting and rendering.
+#	If we wish to use another backend for drawing the graphs, this class should be swapped out.
+class Graph_Drawer:
+	# We currently support 7 different data series each with its own color and plot storage
+	# We can easily add more if needed, however we may have to define the colors as (R,G,B)
+	colors=['g','r','b','c','m','y','w']
+	plot_tracker=[[] for i in range(7)]
+	# This is the list that tracks pending items that are waiting to be drawn
+	plot_queue=[]
+	## 	Function run_loop
+	#	This is the main loop of this ROS Node. It continuously processes incoming QT events (draw requests).
+	#	It replaces the standard ROS spin function. We can only do this in Python, since rospy's spin is just a sleep.
+	def run_loop(self):
+		# Spawn the graph window
+		self.win = CustomGraphicsWindow()
+		self.win.setWindowTitle('Plot Tool')
+		self.graph = self.win.addPlot()
+		self.graph.showGrid(x=True, y=True)
+		# This needs to be called to process QT events (render the window)
+		QtGui.QApplication.instance().processEvents()
+		# Main run loop while ROS is still going
+		while not rospy.is_shutdown():
+			# Check pending queue for new plot items
+			while len(self.plot_queue) > 0:
+				p = self.plot_queue.pop(0)
+				self.plot(p.x_set, p.y_set, p.series, p.append, p.symbol, p.symbol_size)
+				# This renders each plot item as soon as it is added
+				# This causes a slow down if items are plotted at a high frequency (50Hz)
+				# However, this approach does not miss any plot items
+				# If we don't care about missing plot items, we can move this outside
+				# of the while loop to eliminate any slow downs
+				QtGui.QApplication.instance().processEvents()
+	##	Function plot
+	#	Takes Plot_Info items from the pending queue and creates a plot object that can be added to the graph
+	#	@param x_set the list of x coordinates to be plotted
+	#	@param y_set the list of y coordinates to be plotted
+	#	@param series the series that the data belongs to
+	#	@param append True: plot will be added to existing plots in the series. False: plot will replace all previous plots in the series.
+	#	@param symbol Marker types: '-' for line plot, 'o' for circle, 's' for square, 't' for triangle, 'd' for diamond, '+' for cross
+	#	@param symbol_size how big the markers will be (does not affect line plots)
+	def plot(self,x_set,y_set,series,append,symbol,symbol_size):
+		# Default settings will be applied if lacking information
+		if series==None:
+			rospy.logwarn("plot_tool: series not declared.. setting series to 0")
+			series = 0
+		elif series < 0 or series > 6:
+			rospy.logwarn("plot_tool: series must be between 0 and 6.. setting series to 0")
+			series = 0
+		if append==None:
+			rospy.logwarn("plot_tool: append not declared.. setting append to true")
+			append=True
+		if len(x_set) > 0 and len(y_set)>0:
+			if symbol==None:
+				rospy.logwarn("plot_tool: symbol not declared.. setting symbol to -")
+				symbol='-'
+			else:
+				symbol=str(unichr(symbol))
+				if symbol!='o' and symbol!='s' and symbol!='t' and symbol!='d' and symbol!='+' and symbol!='-':
+					rospy.logwarn("plot_tool: symbol is not valid, should be one of { -,o,s,t,d,+ }.. setting symbol to -")
+					symbol='-'
+			if symbol_size==None:
+				rospy.logwarn("plot_tool: symbol_size not declared.. setting symbol_size to 10")
+				symbol_size=10
+		# set plot color based on data series
+		c = self.colors[series]	
+		# clear existing plots if append is False
+		if append==False:
+			plot_list = self.plot_tracker[series]
+			for plot in plot_list:
+				plot.clear()
+			self.plot_tracker[series]=[]
+		# note that we do not always need data, one way to clear plots is to send a request with append = False and no data
+		if x_set!=None and y_set!=None: 
+			# line plots with 1 data point is automatically converted to a scatter plot
+			if (len(x_set) == 1 or len(y_set) == 1) and symbol=='-':
+				rospy.logwarn("plot_tool: can't plot line with single point.. changing symbol to o")
+				symbol='o'
+			if symbol=='-':
+				plot = self.graph.plot(x_set,y_set, pen=c)
+			else:
+				plot = self.graph.plot(x_set,y_set, pen=None, symbolPen=c, symbolBrush=c, symbolSize=symbol_size, symbol=symbol)
+			# save the plot, so we can access it for redrawing or deleting later
+			self.plot_tracker[series].append(plot)
+
+##	Class Plot_Info
+#	This class is just used to organize the plot request parameters into a tidy object
+class Plot_Info:
+	def __init__(self, x_set, y_set, series, append, symbol, symbol_size):
+		self.x_set = x_set
+		self.y_set = y_set
+		self.series = series
+		self.append = append
+		self.symbol = symbol
+		self.symbol_size = symbol_size
+
+## 	Function srv_plot_path
+#	This is the service handler for plotting nav_msgs::Path messages.
+#	@param req this is the standard service request object in ROS
+def srv_plot_path(req):
+	# Extract the plot data out of the nav_msgs::Path message 
+	# and organize it into a list of x coordinates and a list of y coordinates
+	data = req.msg
+	if data==None:
+		x_set=None
+		y_set=None
+	else:
+		pose_list = data.poses
+		N=len(pose_list)
+		i=0
+		x_set=[]
+		y_set=[]
+		while i<N:
+			if (pose_list[i].pose.position.x != None and pose_list[i].pose.position.y != None):
+				x_set.append(pose_list[i].pose.position.x)
+				y_set.append(pose_list[i].pose.position.y)
+			i=i+1
+	# Create a Plot_Info object with request parameters and add it to the pending queue (for drawing)
+	graph_obj.plot_queue.append(Plot_Info(x_set,y_set,req.series,req.append,req.symbol,req.symbol_size))
+	# Return acknowledgement that the service is complete
+	return PlotPathResponse()
+
+## 	Function srv_plot_pose
+#	This is the service handler for plotting geometry_msgs::Pose messages.
+#	@param req this is the standard service request object in ROS
+def srv_plot_pose(req):
+	# Extract the plot data out of the geometry_msgs::Pose message.
+	# and organize it into a list of x coordinates and a list of y coordinates
+	data = req.msg
+	if data==None:
+		x_set=None
+		y_set=None
+	else:
+		if (data.position.x != None and data.position.y != None):
+			x_set=[data.position.x]
+			y_set=[data.position.y]
+	# Create a Plot_Info object with request parameters and add it to the pending queue (for drawing)
+	graph_obj.plot_queue.append(Plot_Info(x_set,y_set,req.series,req.append,req.symbol,req.symbol_size))
+	# Return acknowledgement that the service is complete
+	return PlotPoseResponse()
+            
+##	Function node_setup
+#	This function sets up the ROS Node and registers the ROS services it provides
+def node_setup():
+	# Create a global Graph_Drawer instance for everything to use/draw on
+	# This is necessary because QTCore and QTGui does not like to be controlled from multiple threads
+	# However, rospy spawns different threads to handle service requests and callbacks
+	# To avoid rendering issues, we are using the queue system in Graph_Drawer for plot requests
+	global graph_obj
+	graph_obj = Graph_Drawer()
+	# Start the ROS Node and register the services offered
+	rospy.init_node('plot_path')
+	drawLine_srv = rospy.Service("/plot_tool/draw_path", PlotPath, srv_plot_path)
+	drawPoint_srv = rospy.Service("/plot_tool/draw_pose", PlotPose, srv_plot_pose)
+	# Kick off the main run loop in the Graph_Drawer instance
+	graph_obj.run_loop()
+
+# 	Point of entry into the program
+if __name__ == '__main__':
+	node_setup()
+
+
+
